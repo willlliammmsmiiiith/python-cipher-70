44595879295b7c88b0908436c94a3d3f23ce0494
willlliammmsmiiiith <56@felix-au.me>
2026-08-04 04:23:54 +0530

    Update cipher.py - Step 10

--- cipher.py
+++ cipher.py
@@ -3,3 +3,6 @@ def encrypt(text, shift):
     for char in text:
         if char.isalpha():
             start = ord('A') if char.isupper() else ord('a')
+            res += chr((ord(char) - start + shift) % 26 + start)
+        else: res += char
+    return res
